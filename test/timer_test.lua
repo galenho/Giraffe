72434eaa62363d10723c74a27bf0c224cdbd1c6c
galenho <galenho@126.com>
2022-03-02 10:50:02 +0800

    去掉test目录

--- test/timer_test.lua
+++
@@ -1,8 +0,0 @@
-
-
-fun_timer_callback = function(timer_id, param)
-	print("timer_id" .. "----" .. timer_id)
-	--crossover.remove_timer(timer_id)
-end
-
-crossover.add_timer(1000, fun_timer_callback, {a = 5, b = 6})
